25e8cbeb63ddb8a4b9f7eaf433bf1495d6f35429
Sorokovsky <Sorokovskys@ukr.net>
2025-02-05 22:50:51 +0200

    add tests

---
+++ UiCommands.Tests/Test1.cs
@@ -0,0 +1,16 @@
+﻿using UiCommands.Core.Contexts;
+
+namespace UiCommands.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethod1()
+    {
+        const string titleContext = "Головне меню.";
+        var context = new CommandsContext(titleContext);
+        Assert.AreEqual(0, context.Number);
+        Assert.AreEqual(titleContext, context.Title);
+    }
+}
